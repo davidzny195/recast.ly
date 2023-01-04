8adf9eda115bd6d21d237a4434deb6c88f2c3734
Jacob Fink <jacobifink@gmail.com>
2023-01-03 17:34:03 -0800

    Passes tests up to using ajax, working on searchYoutube now.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,42 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+import Search from './Search.js';
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+import exampleVideoData from '../data/exampleVideoData.js';
+import searchYouTube from '../lib/searchYouTube.js';
+
+const App = () => {
+  const [currentVideo, setVideo] = React.useState(exampleVideoData[0]);
+  const [currentList, setList] = React.useState(exampleVideoData);
+
+  const handleVideoChange = event => {
+    let title = event.target.innerText;
+    let video = exampleVideoData.find((data) => data.snippet.title === title);
+    setVideo(video);
+    // setVideo(event.target.value);
+  };
+
+  const test = () => {
+    console.log(searchYoutube('hello'));
+  };
+
+
+  return (
+    <div>
+      <nav className="navbar">
+        <Search />
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <VideoPlayer video={currentVideo} />
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={currentList} onSelect={handleVideoChange} />
+        </div>
+        <button onClick={test}>Click</button>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -4,8 +4,24 @@ $.ajaxPrefilter(function (settings, _, jqXHR) {
   jqXHR.setRequestHeader('Authorization', API_KEY);
 });
 
-var searchYouTube = (query, callback) => {
+const searchYouTube = (query = '', callback) => {
   // TODO
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      key: YOUTUBE_API_KEY,
+      q: query,
+      videoEmbeddable: true,
+      part: 'snippet',
+      type: 'video',
+      maxResults: 10
+    },
+    success: console.log('success'),
+    error: callback || function(error) {
+      console.error('Recast.ly: Failed to fetch messages', error);
+    }
+  });
 };
 
 export default searchYouTube;
